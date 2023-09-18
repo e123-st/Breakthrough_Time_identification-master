2cec99da9edb544901884d0fe56746a78574f4fa
e123-st <535564864@qq.com>
2023-09-18 08:46:29 +0800

    Update

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name='Breakthrough_Time_identification-master',
+      version='1.0.0',
+      packages=find_packages(),
+      description='This is a packsge for breakthrough curve calculation',
+      author='Yizhen Situ & Qiao Zhiwei'
+      author_email="zqiao@gzhu.edu.cn")
